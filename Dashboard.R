d1f89aedf4a0a0bef76254a6432fec9531a87745
philipp <64225416+philipp-code@users.noreply.github.com>
2021-05-06 08:45:10 +0200

    adds InputFields for PSO Algorithm

--- Dashboard.R
+++ Dashboard.R
@@ -3,89 +3,105 @@
 ####
 library(shiny)
 library(DT)
-View(iris)
 #UI
 
-ui <- navbarPage("Test DHBW", 
-                 tabPanel("Vizualization of Algorithm", icon=icon("info"),
-                          h3("Das ist ein Test"),
-                          sidebarLayout(
-                            sidebarPanel(
-                              selectInput(
-                                inputId = "xcol",
-                                label = "X Variable",
-                                choices = names(iris)
-                              ),
-                              selectInput(
-                                inputId = "ycol",
-                                label = "Y Variable",
-                                choices = c("Choose a variable" = "", names(iris))
-                              ),
-                              numericInput(
-                                inputId = "clusters",
-                                label = "Clusters count",
-                                value = 3,
-                                min = 1,
-                                max = 9
-                              ),
-                              sliderInput(
-                                inputId = "iter",
-                                label = "PSO Iteration",
-                                value = 1,
-                                min = 1,
-                                max = 101,
-                                step = 1,
-                                animate = animationOptions(interval = 100)
-                              )
-                            ),
-                            mainPanel(
-                              # h4(textOutput("render_optim")),
-                              plotOutput("render_particles", width = 500, height = 500)
-                            )
-                          )
-                 ),
-                 tabPanel("Functionality of Algorithm", icon=icon("robot"),
-                          h3("Hier wird die Funktionalitaet des Algoithmus beschrieben"),
-                          hr(),
-                          br(),
-                          DT::dataTableOutput("myTable")
-                 )
+ui <- navbarPage(
+  "Particle Swarm Optimization",
+  # tabPanel("Functionality of Algorithm", icon=icon("info"),
+  #          h3("Hier wird die Funktionalitaet des Algoithmus beschrieben"),
+  #          hr(),
+  #          br(),
+  #          #DT::dataTableOutput("myTable")
+  # ),
+  tabPanel(
+    "Visualization of Algorithm",
+    icon = icon("glyphicon glyphicon-eye-open", lib = "glyphicon"),
+    h3("Interactive Graph"),
+    sidebarLayout(
+      sidebarPanel(
+        numericInput(
+          inputId = "inertia",
+          label = "Inerita",
+          value = 1,
+          min = 0,
+          max = 1,
+          step = 0.1
+        ),
+        selectInput(
+          inputId = "function_selected",
+          label = "Base Function",
+          choices = list("f1" = "f1", "f2" = "f2")
+        ),
+        numericInput(
+          inputId = "n_particles",
+          label = "Number of Particles",
+          value = 100,
+          min = 1,
+          max = 1000
+        ),
+        selectInput(
+          inputId = "auto_coef",
+          label = "Auto-Coefficient",
+          choices = list("On" = TRUE, "Off" = FALSE),
+          selected = TRUE
+        ),
+        selectInput(
+          inputId = "norm_arrows",
+          label = "Normalize Arrows",
+          choices = list("On" = TRUE, "Off" = FALSE),
+          selected = FALSE
+        ),
+        sliderInput(
+          inputId = "iter",
+          label = "Iterations",
+          value = 1,
+          min = 1,
+          max = 101,
+          step = 1,
+          animate = animationOptions(interval = 100)
+        )
+        
+      ),
+      mainPanel(# h4(textOutput("render_optim")),
+        plotOutput(
+          "render_particles", width = 500, height = 500
+        ))
+    )
+  )
 )
 
 #================
 #Server
 
 server <- function(input, output, session) {
-  pso = init_pso(100)
   
-  selectedData <- reactive({
-    iris[, c(input$xcol, input$ycol)]
+  max_iterations <- 100
+  
+  #initalize pso
+  pso  = reactive({
+    
+    #returns message if test fails
+    validate(need(!is.null(input$inertia), "inertia is null"))
+    #initial plot (iter = 0)
+    init_pso(
+      max_iterations,
+      input$n_particles,
+      as.logical(input$auto_coef),
+      input$inertia,
+      as.logical(input$norm_arrows)
+    )
   })
   
-  clusters <- reactive({
-    kmeans(selectedData(), input$clusters)
-  })
-  
+  #plot current pso state
   pso_output = reactive({
-    run_pso(input$iter, 100)
-  })
-  
-  output$PlotKMeans <- renderPlot({
-    
-    req(input$xcol)
-    req(input$ycol)
-    req(input$clusters)
-    
-    par(mar = c(5.1, 4.1, 0, 1))
-    plot(selectedData(),
-         col = clusters()$cluster,
-         pch = 20, cex = 3)
-    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
-    
-  })
-  
-  output$myTable = DT::renderDataTable({
-    iris
+    run_pso(
+      input$iter,
+      max_iterations,
+      input$n_particles,
+      as.logical(input$auto_coef),
+      input$inertia ,
+      as.logical(input$norm_arrows)
+    )
   })
   
   output$render_optim = renderText({
@@ -93,13 +109,14 @@ server <- function(input, output, session) {
   })
   
   output$render_particles = renderPlot({
-    if (input$iter == pso$iter+1) {
-      pso$next_i()
+    if (input$iter == pso()$iter + 1) {
+      #moves particles, updates bests, updates coefficients 
+      pso()$next_i()
     } else {
-      pso <<- pso_output()
+      pso() <- pso_output()
     }
     # plot(pso$particles[ ,1], pso$particles[ ,2], xlim=c(-5, 5), ylim=c(-5, 5))
-    pso$plot_state()
+    pso()$plot_state()
   })
 }
 
