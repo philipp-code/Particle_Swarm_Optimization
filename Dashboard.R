59c0603eb807bf4364d4e46e8a8078ac4f573ecb
Frederik Dammeier <frederik.dammeier@gmx.de>
2021-05-06 12:23:12 +0200

    :bug: bei nicht normalisierten Pfeilen sollte die viz funktionieren

--- Dashboard.R
+++ Dashboard.R
@@ -203,20 +203,22 @@ server <- function(input, output, session) {
   #================= "VISUALIZATION OF ALGORITHM" functionality ============================  
     
   max_iterations <- 100
-  
   #initalize pso
-  pso  = reactive({
-    #returns message if test fails
-    validate(need(!is.null(input$inertia), "inertia is null"))
-    #initial plot (iter = 0)
-    init_pso(
-      max_iterations,
-      input$n_particles,
-      as.logical(input$auto_coef),
-      input$inertia,
-      as.logical(input$norm_arrows)
-    )
-  })
+  pso = NULL
+  
+#    reactive({
+#    #returns message if test fails
+#    validate(need(!is.null(input$inertia), "inertia is null"))
+#    #initial plot (iter = 0)
+#   init_pso(
+#      max_iterations,
+#      input$n_particles,
+#      as.logical(input$auto_coef),
+#      input$inertia,
+#      as.logical(input$norm_arrows)
+#    )
+#  })
+  
   
   #plot current pso state
   pso_output = reactive({
@@ -235,14 +237,16 @@ server <- function(input, output, session) {
   })
   
   output$render_particles = renderPlot({
-    if (input$iter == pso()$iter + 1) {
+    if(is.null(pso)){
+      pso <<- pso_output() 
+    } else if (input$iter == pso$iter + 1) {
       #moves particles, updates bests, updates coefficients 
-      pso()$next_i()
+      pso$next_i()
     } else {
-      pso() <<- pso_output()
+      pso <<- pso_output()
     }
     # plot(pso$particles[ ,1], pso$particles[ ,2], xlim=c(-5, 5), ylim=c(-5, 5))
-    pso()$plot_state()
+    pso$plot_state()
   })
   
   #================= Gallery functionality ============================
